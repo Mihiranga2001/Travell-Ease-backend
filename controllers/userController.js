c337b9076e90e9a8bcf77b4a12f25930ce7a675b
Gaurawa Wickramasinghe <gaurawamihiranga@gmail.com>
2026-05-22 20:05:31 +0530

    Password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export function getUsers(req,res){
     User.find().then((users) => {
@@ -12,8 +13,24 @@ export function getUsers(req,res){
 }
 
 export function createUser(req,res){
+    const data = req.body;
 
-    const user = new User(req.body);
+    const hashedPassword = bcrypt.hashSync(data.password,10);
+
+    const user = new User({
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+        role: data.role,
+        phoneNumber: data.phoneNumber,
+        profilePhoto: data.profilePhoto,
+        bio: data.bio,
+        location: data.location,
+        interests: data.interests,
+        isVerified: data.isVerified,
+        isBlocked: data.isBlocked
+    });
+
     user.save().then(() => {
         res.json({
             message: "User created successfully"
@@ -23,6 +40,40 @@ export function createUser(req,res){
         res.status(500).json({
             message: "Failed to create user"
         });
+    });
+}
+
+export function loginUser(req, res) {
+	const email = req.body.email;
+	const password = req.body.password;
+	User.find({ email: email }).then((users) => {
+		if (users[0] == null) {
+			res.status(404).json({
+				message: "User not found",
+			});
+		} else {
+			const user = users[0];
+
+			if (user.isBlocked) {
+				res.status(403).json({
+					message: "User is blocked. Contact admin.",
+				});
+				return;
+			}
+
+			const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+            if (isPasswordCorrect) {
+                res.json({
+                    message: "Login successful",
+                    user: user,
+                });
+            } else {
+                res.status(401).json({
+                    message: "Invalid credentials",
+                });
+            }
+        }
     });
 }
 
